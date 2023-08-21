a7dc5b5361622a28314e7290de0e01d2546e5547
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-08-20 22:09:06 -0700

    Add component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Message from "./Message";
-
+import Navbar from "../components/Navbar";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
 export default function App() {
   return (
     <div>
       {" "}
       <Message />
+      <Navbar />
+      <Body />
+      <Footer />
     </div>
   );
 }
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,3 +1,3 @@
 export default function message() {
-  return <h1>Hello world</h1>;
+  return;
 }
