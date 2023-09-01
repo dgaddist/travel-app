6f14adeccdedb60493fbe3fe3723809eb12e442d
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-09-01 14:49:47 -0700

    create child component activities b to pass props create data file to pass with map()

--- src/Activities.jsx
+++ src/Activities.jsx
@@ -1,83 +1,34 @@
 import Card from "react-bootstrap/Card";
+import activitiesdata from "./activitiesdata";
+import ActivityBody from "./Activitiesb";
 
 export default function Activities() {
+  // <Hero />
+
   return (
     <div>
-      <div className="Activity-title">Highly Rated Activities</div>
-      <div className="Activity-cards">
-        <div>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="hiking.jpg" />
-            <Card.Body>
-              <Card.Title>
-                <img className="star-icon" src="icon-star.png" width="15px" />{" "}
-                <span className="raiting">4.9 </span>
-                <span className="reviews">(500)</span>
-              </Card.Title>
-              <Card.Text>
-                <div> Hiking Adventure in Rocky Mountain National Park</div>
-                <div>
-                  From $<span>139</span> per group
-                </div>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="zipline.jpg" height="285px" />
-            <Card.Body>
-              <Card.Title>
-                <img className="star-icon" src="icon-star.png" width="15px" />{" "}
-                <span className="raiting">4.7 </span>
-                <span className="reviews">(480)</span>
-              </Card.Title>
-              <Card.Text>
-                <div> Canopy Zipline in Puerto Vallarta</div>
-                <div>
-                  From $<span>139</span> per group
-                </div>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="boat.jpg" />
-            <Card.Body>
-              <Card.Title>
-                <img className="star-icon" src="icon-star.png" width="15px" />{" "}
-                <span className="raiting">4.8 </span>
-                <span className="reviews">(350)</span>
-              </Card.Title>
-              <Card.Text>
-                <div> Sunset Dinner Cruise in Nassau</div>
-                <div>
-                  From $<span>116</span> per group
-                </div>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="ski.jpg" />
-            <Card.Body>
-              <Card.Title>
-                <img className="star-icon" src="icon-star.png" width="15px" />{" "}
-                <span className="raiting">4.9 </span>
-                <span className="reviews">(520)</span>
-              </Card.Title>
-              <Card.Text>
-                <div> Group Jet Ski Rentals in Miami </div>
-                <div>
-                  From $<span>145</span> per group
-                </div>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
+      <ActivityBody
+        image="./hiking.jpg"
+        rating="4.9"
+        reviews="500 "
+        activity="Hiking Adventure in Rocky Mountain National Park"
+        starIcon="./icon-star.png"
+        price="139"
+      />
     </div>
   );
 }
+
+// const activtiycards = activitiesdata.map((item) => {
+//   return (
+//     <ActivityBody />
+//       img={item.image}
+//       activity={item.name}
+//       rating={item.rating}
+//       reviews={item.reviews}
+//       price={item.price}
+//       icon={item.starIcon}
+//     />
+//   );
+// });
+// {activtiycards}
---
+++ src/activitiesdata.jsx
@@ -0,0 +1,34 @@
+export default [
+  {
+    image: "./hiking.jpg",
+    rating: "4.9",
+    reviews: "500 ",
+    activity: "Hiking Adventure in Rocky Mountain National Park",
+    starIcon: "./icon-star.png",
+    price: "139",
+  },
+  {
+    image: "./zipline.jpg",
+    rating: "4.7",
+    reviews: "480 ",
+    activity: "Canopy Zipline in Puerto Vallarta",
+    starIcon: "./icon-star.png",
+    price: "139",
+  },
+  {
+    image: "./boat.jpg",
+    rating: "4.8",
+    reviews: "350",
+    activity: " Sunset Dinner Cruise in Nassau",
+    starIcon: "./icon-star.png",
+    price: "116",
+  },
+  {
+    image: "./ski.jpg",
+    rating: "4.9",
+    reviews: "520 ",
+    activity: "Group Jet Ski Rentals in Miami",
+    starIcon: "./icon-star.png",
+    pice: "145",
+  },
+];
